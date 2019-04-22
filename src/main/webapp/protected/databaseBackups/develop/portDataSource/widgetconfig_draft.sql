c193fdacc893f830d18bbf7a05421e1a1efc436a
joewhite101 <j.white@entando.com>
2019-04-22 15:44:46 +0200

    Adding example

---
+++ src/main/webapp/protected/databaseBackups/develop/portDataSource/widgetconfig_draft.sql
@@ -0,0 +1,2 @@
+INSERT INTO widgetconfig_draft (pagecode,framepos,widgetcode,config) VALUES ('test',0,'testw',NULL);
+INSERT INTO widgetconfig_draft (pagecode,framepos,widgetcode,config) VALUES ('login',0,'login_form',NULL);
---
+++ src/main/webapp/protected/databaseBackups/develop/servDataSource/actionlogrelations.sql
@@ -0,0 +1,7 @@
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (1,'free');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (2,'free');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (3,'free');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (4,'administrators');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (5,'administrators');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (6,'administrators');
+INSERT INTO actionlogrelations (recordid,refgroup) VALUES (7,'free');
